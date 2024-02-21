7096150692bdae21f936d072e6a9aa1130e33c3c
anikpramanikcse <anikpramanikcse@gmail.com>
2024-02-21 04:25:16 -0500

    Printing the K shapes

--- main_pacfl.py
+++ main_pacfl.py
@@ -210,9 +210,6 @@ for i in range(args.num_users):
 clients = []
 U_clients = []
 
-print(traindata_cls_ratio)
-Print("new stuff")
-
 K = args.n_basis
 #K = 5
 for idx in range(args.num_users):
@@ -258,6 +255,7 @@ for idx in range(args.num_users):
     U_temp = []
     for j in range(nlabels):
         local_ds1 = train_ds_local.data[idxs_local[cnt:cnt+cnt_labels[j]]]
+        print("For client, label, datashape :", idx, j, local_ds1.shape )
         local_ds1 = local_ds1.reshape(cnt_labels[j], -1)
         local_ds1 = local_ds1.T
         if type(train_ds_local.target[idxs_local[cnt:cnt+cnt_labels[j]]]) == torch.Tensor:
@@ -278,13 +276,15 @@ for idx in range(args.num_users):
             u1_temp, sh1_temp, vh1_temp = np.linalg.svd(local_ds1, full_matrices=False)
             u1_temp=u1_temp/np.linalg.norm(u1_temp, ord=2, axis=0)
             U_temp.append(u1_temp[:, 0:K])
+            print(K)
+            print("For client, label, datashape :", idx, j, U_temp[-1].shape )
+            print(" ")
             
         cnt+=cnt_labels[j]
         
     #U_temp = [u1_temp[:, 0:K], u2_temp[:, 0:K]]
     U_clients.append(copy.deepcopy(np.hstack(U_temp)))
-    
-    print(f'Shape of U: {U_clients[-1].shape}')
+    #print(f'Shape of U: {U_clients[-1].shape}')
     
     clients.append(Client_ClusterFL(idx, copy.deepcopy(users_model[idx]), args.local_bs, args.local_ep, 
                args.lr, args.momentum, args.device, train_dl_local, test_dl_local))
@@ -328,229 +328,229 @@ print(f'Clients: Cluster_ID \n{clients_clust_id}')
 
 ###################################### Federation 
 
-loss_train = []
+# loss_train = []
 
-init_tracc_pr = []  # initial train accuracy for each round 
-final_tracc_pr = [] # final train accuracy for each round 
+# init_tracc_pr = []  # initial train accuracy for each round 
+# final_tracc_pr = [] # final train accuracy for each round 
 
-init_tacc_pr = []  # initial test accuarcy for each round 
-final_tacc_pr = [] # final test accuracy for each round
+# init_tacc_pr = []  # initial test accuarcy for each round 
+# final_tacc_pr = [] # final test accuracy for each round
 
-init_tloss_pr = []  # initial test loss for each round 
-final_tloss_pr = [] # final test loss for each round 
+# init_tloss_pr = []  # initial test loss for each round 
+# final_tloss_pr = [] # final test loss for each round 
 
-clients_best_acc = [0 for _ in range(args.num_users)]
-w_locals, loss_locals = [], []
+# clients_best_acc = [0 for _ in range(args.num_users)]
+# w_locals, loss_locals = [], []
 
-init_local_tacc = []       # initial local test accuracy at each round 
-final_local_tacc = []      # final local test accuracy at each round 
+# init_local_tacc = []       # initial local test accuracy at each round 
+# final_local_tacc = []      # final local test accuracy at each round 
 
-init_local_tloss = []      # initial local test loss at each round 
-final_local_tloss = []     # final local test loss at each round 
+# init_local_tloss = []      # initial local test loss at each round 
+# final_local_tloss = []     # final local test loss at each round 
 
-ckp_avg_tacc = []
-ckp_avg_best_tacc = []
+# ckp_avg_tacc = []
+# ckp_avg_best_tacc = []
 
-w_glob_per_cluster = [copy.deepcopy(initial_state_dict) for _ in range(len(clusters))]
+# w_glob_per_cluster = [copy.deepcopy(initial_state_dict) for _ in range(len(clusters))]
 
-users_best_acc = [0 for _ in range(args.num_users)]
-best_glob_acc = [0 for _ in range(len(clusters))]
+# users_best_acc = [0 for _ in range(args.num_users)]
+# best_glob_acc = [0 for _ in range(len(clusters))]
 
-print_flag = False
-for iteration in range(args.rounds):
+# print_flag = False
+# for iteration in range(args.rounds):
         
-    m = max(int(args.frac * args.num_users), 1)
-    idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+#     m = max(int(args.frac * args.num_users), 1)
+#     idxs_users = np.random.choice(range(args.num_users), m, replace=False)
     
-    #idxs_users = comm_users[iteration]
+#     #idxs_users = comm_users[iteration]
     
-    print(f'###### ROUND {iteration+1} ######')
-    print(f'Clients {idxs_users}')
+#     print(f'###### ROUND {iteration+1} ######')
+#     print(f'Clients {idxs_users}')
     
-    idx_clusters_round = {}
-    for idx in idxs_users:
-        idx_cluster = clients_clust_id[idx]
-        idx_clusters_round[idx_cluster] = []
+#     idx_clusters_round = {}
+#     for idx in idxs_users:
+#         idx_cluster = clients_clust_id[idx]
+#         idx_clusters_round[idx_cluster] = []
         
-    for idx in idxs_users:
-        idx_cluster = clients_clust_id[idx]
-        idx_clusters_round[idx_cluster].append(idx)
+#     for idx in idxs_users:
+#         idx_cluster = clients_clust_id[idx]
+#         idx_clusters_round[idx_cluster].append(idx)
         
-        clients[idx].set_state_dict(copy.deepcopy(w_glob_per_cluster[idx_cluster])) 
+#         clients[idx].set_state_dict(copy.deepcopy(w_glob_per_cluster[idx_cluster])) 
             
-        loss, acc = clients[idx].eval_test()        
+#         loss, acc = clients[idx].eval_test()        
             
-        init_local_tacc.append(acc)
-        init_local_tloss.append(loss)
+#         init_local_tacc.append(acc)
+#         init_local_tloss.append(loss)
             
-        loss = clients[idx].train(is_print=False)
+#         loss = clients[idx].train(is_print=False)
                         
-        loss_locals.append(copy.deepcopy(loss))
+#         loss_locals.append(copy.deepcopy(loss))
                        
-        loss, acc = clients[idx].eval_test()
+#         loss, acc = clients[idx].eval_test()
         
-        if acc > clients_best_acc[idx]:
-            clients_best_acc[idx] = acc
+#         if acc > clients_best_acc[idx]:
+#             clients_best_acc[idx] = acc
         
-        final_local_tacc.append(acc)
-        final_local_tloss.append(loss)           
+#         final_local_tacc.append(acc)
+#         final_local_tloss.append(loss)           
     
-    total_data_points = {}
-    for k in idx_clusters_round.keys(): 
-        temp_sum = []
-        for r in idx_clusters_round[k]: 
-            temp_sum.append(len(net_dataidx_map[r]))
+#     total_data_points = {}
+#     for k in idx_clusters_round.keys(): 
+#         temp_sum = []
+#         for r in idx_clusters_round[k]: 
+#             temp_sum.append(len(net_dataidx_map[r]))
             
-        total_data_points[k] = sum(temp_sum)
+#         total_data_points[k] = sum(temp_sum)
     
-    fed_avg_freqs = {}
-    for k in idx_clusters_round.keys():
-        fed_avg_freqs[k] = []
-        for r in idx_clusters_round[k]: 
-            ratio = len(net_dataidx_map[r]) / total_data_points[k]
-            fed_avg_freqs[k].append(copy.deepcopy(ratio))
+#     fed_avg_freqs = {}
+#     for k in idx_clusters_round.keys():
+#         fed_avg_freqs[k] = []
+#         for r in idx_clusters_round[k]: 
+#             ratio = len(net_dataidx_map[r]) / total_data_points[k]
+#             fed_avg_freqs[k].append(copy.deepcopy(ratio))
     
-    for k in idx_clusters_round.keys():
-        w_locals = []
-        for el in idx_clusters_round[k]:
-            w_locals.append(copy.deepcopy(clients[el].get_state_dict()))
+#     for k in idx_clusters_round.keys():
+#         w_locals = []
+#         for el in idx_clusters_round[k]:
+#             w_locals.append(copy.deepcopy(clients[el].get_state_dict()))
             
-        ww = FedAvg(w_locals, weight_avg = fed_avg_freqs[k])
-        w_glob_per_cluster[k] = copy.deepcopy(ww)
-        net_glob.load_state_dict(copy.deepcopy(ww))
-        _, acc = eval_test(net_glob, args, test_dl_global)
-        if acc > best_glob_acc[k]:
-            best_glob_acc[k] = acc 
+#         ww = FedAvg(w_locals, weight_avg = fed_avg_freqs[k])
+#         w_glob_per_cluster[k] = copy.deepcopy(ww)
+#         net_glob.load_state_dict(copy.deepcopy(ww))
+#         _, acc = eval_test(net_glob, args, test_dl_global)
+#         if acc > best_glob_acc[k]:
+#             best_glob_acc[k] = acc 
     
-    # print loss
-    loss_avg = sum(loss_locals) / len(loss_locals)
-    avg_init_tloss = sum(init_local_tloss) / len(init_local_tloss)
-    avg_init_tacc = sum(init_local_tacc) / len(init_local_tacc)
-    avg_final_tloss = sum(final_local_tloss) / len(final_local_tloss)
-    avg_final_tacc = sum(final_local_tacc) / len(final_local_tacc)
+#     # print loss
+#     loss_avg = sum(loss_locals) / len(loss_locals)
+#     avg_init_tloss = sum(init_local_tloss) / len(init_local_tloss)
+#     avg_init_tacc = sum(init_local_tacc) / len(init_local_tacc)
+#     avg_final_tloss = sum(final_local_tloss) / len(final_local_tloss)
+#     avg_final_tacc = sum(final_local_tacc) / len(final_local_tacc)
          
-    print('## END OF ROUND ##')
-    template = 'Average Train loss {:.3f}'
-    print(template.format(loss_avg))
+#     print('## END OF ROUND ##')
+#     template = 'Average Train loss {:.3f}'
+#     print(template.format(loss_avg))
     
-    template = "AVG Init Test Loss: {:.3f}, AVG Init Test Acc: {:.3f}"
-    print(template.format(avg_init_tloss, avg_init_tacc))
+#     template = "AVG Init Test Loss: {:.3f}, AVG Init Test Acc: {:.3f}"
+#     print(template.format(avg_init_tloss, avg_init_tacc))
     
-    template = "AVG Final Test Loss: {:.3f}, AVG Final Test Acc: {:.3f}"
-    print(template.format(avg_final_tloss, avg_final_tacc))
+#     template = "AVG Final Test Loss: {:.3f}, AVG Final Test Acc: {:.3f}"
+#     print(template.format(avg_final_tloss, avg_final_tacc))
     
-    print_flag = False
-    if iteration < 60:
-        print_flag = True
-    elif iteration%args.print_freq == 0: 
-        print_flag = True
+#     print_flag = False
+#     if iteration < 60:
+#         print_flag = True
+#     elif iteration%args.print_freq == 0: 
+#         print_flag = True
         
-    if print_flag:
-        print('--- PRINTING ALL CLIENTS STATUS ---')
-        current_acc = []
-        for k in range(args.num_users):
-            loss, acc = clients[k].eval_test() 
-            current_acc.append(acc)
+#     if print_flag:
+#         print('--- PRINTING ALL CLIENTS STATUS ---')
+#         current_acc = []
+#         for k in range(args.num_users):
+#             loss, acc = clients[k].eval_test() 
+#             current_acc.append(acc)
             
-            template = ("Client {:3d}, labels {}, count {}, best_acc {:3.3f}, current_acc {:3.3f} \n")
-            print(template.format(k, traindata_cls_counts[k], clients[k].get_count(),
-                                  clients_best_acc[k], current_acc[-1]))
+#             template = ("Client {:3d}, labels {}, count {}, best_acc {:3.3f}, current_acc {:3.3f} \n")
+#             print(template.format(k, traindata_cls_counts[k], clients[k].get_count(),
+#                                   clients_best_acc[k], current_acc[-1]))
             
-        template = ("Round {:1d}, Avg current_acc {:3.3f}, Avg best_acc {:3.3f}")
-        print(template.format(iteration+1, np.mean(current_acc), np.mean(clients_best_acc)))
+#         template = ("Round {:1d}, Avg current_acc {:3.3f}, Avg best_acc {:3.3f}")
+#         print(template.format(iteration+1, np.mean(current_acc), np.mean(clients_best_acc)))
         
-        ckp_avg_tacc.append(np.mean(current_acc))
-        ckp_avg_best_tacc.append(np.mean(clients_best_acc))
+#         ckp_avg_tacc.append(np.mean(current_acc))
+#         ckp_avg_best_tacc.append(np.mean(clients_best_acc))
     
-    print('----- Analysis End of Round -------')
-    for idx in idxs_users:
-        print(f'Client {idx}, Count: {clients[idx].get_count()}, Labels: {traindata_cls_counts[idx]}')
+#     print('----- Analysis End of Round -------')
+#     for idx in idxs_users:
+#         print(f'Client {idx}, Count: {clients[idx].get_count()}, Labels: {traindata_cls_counts[idx]}')
        
-    print('')
-    print(f'Clusters {idx_clusters_round}')
-    print('')
+#     print('')
+#     print(f'Clusters {idx_clusters_round}')
+#     print('')
     
-    loss_train.append(loss_avg)
+#     loss_train.append(loss_avg)
     
-    init_tacc_pr.append(avg_init_tacc)
-    init_tloss_pr.append(avg_init_tloss)
+#     init_tacc_pr.append(avg_init_tacc)
+#     init_tloss_pr.append(avg_init_tloss)
     
-    final_tacc_pr.append(avg_final_tacc)
-    final_tloss_pr.append(avg_final_tloss)
+#     final_tacc_pr.append(avg_final_tacc)
+#     final_tloss_pr.append(avg_final_tloss)
     
-    #break;
-    ## clear the placeholders for the next round 
-    loss_locals.clear()
-    init_local_tacc.clear()
-    init_local_tloss.clear()
-    final_local_tacc.clear()
-    final_local_tloss.clear()
+#     #break;
+#     ## clear the placeholders for the next round 
+#     loss_locals.clear()
+#     init_local_tacc.clear()
+#     init_local_tloss.clear()
+#     final_local_tacc.clear()
+#     final_local_tloss.clear()
     
-    ## calling garbage collector 
-    gc.collect()
+#     ## calling garbage collector 
+#     gc.collect()
     
-############################### Saving Training Results 
-# with open(path+str(args.trial)+'_loss_train.npy', 'wb') as fp:
-#     loss_train = np.array(loss_train)
-#     np.save(fp, loss_train)
+# ############################### Saving Training Results 
+# # with open(path+str(args.trial)+'_loss_train.npy', 'wb') as fp:
+# #     loss_train = np.array(loss_train)
+# #     np.save(fp, loss_train)
     
-# with open(path+str(args.trial)+'_init_tacc_pr.npy', 'wb') as fp:
-#     init_tacc_pr = np.array(init_tacc_pr)
-#     np.save(fp, init_tacc_pr)
+# # with open(path+str(args.trial)+'_init_tacc_pr.npy', 'wb') as fp:
+# #     init_tacc_pr = np.array(init_tacc_pr)
+# #     np.save(fp, init_tacc_pr)
     
-# with open(path+str(args.trial)+'_init_tloss_pr.npy', 'wb') as fp:
-#     init_tloss_pr = np.array(init_tloss_pr)
-#     np.save(fp, init_tloss_pr)
+# # with open(path+str(args.trial)+'_init_tloss_pr.npy', 'wb') as fp:
+# #     init_tloss_pr = np.array(init_tloss_pr)
+# #     np.save(fp, init_tloss_pr)
     
-# with open(path+str(args.trial)+'_final_tacc_pr.npy', 'wb') as fp:
-#     final_tacc_pr = np.array(final_tacc_pr)
-#     np.save(fp, final_tacc_pr)
+# # with open(path+str(args.trial)+'_final_tacc_pr.npy', 'wb') as fp:
+# #     final_tacc_pr = np.array(final_tacc_pr)
+# #     np.save(fp, final_tacc_pr)
     
-# with open(path+str(args.trial)+'_final_tloss_pr.npy', 'wb') as fp:
-#     final_tloss_pr = np.array(final_tloss_pr)
-#     np.save(fp, final_tloss_pr)
+# # with open(path+str(args.trial)+'_final_tloss_pr.npy', 'wb') as fp:
+# #     final_tloss_pr = np.array(final_tloss_pr)
+# #     np.save(fp, final_tloss_pr)
     
-# with open(path+str(args.trial)+'_best_glob_w.pt', 'wb') as fp:
-#     torch.save(best_glob_w, fp)
-############################### Printing Final Test and Train ACC / LOSS
-test_loss = []
-test_acc = []
-train_loss = []
-train_acc = []
+# # with open(path+str(args.trial)+'_best_glob_w.pt', 'wb') as fp:
+# #     torch.save(best_glob_w, fp)
+# ############################### Printing Final Test and Train ACC / LOSS
+# test_loss = []
+# test_acc = []
+# train_loss = []
+# train_acc = []
 
-for idx in range(args.num_users):        
-    loss, acc = clients[idx].eval_test()
+# for idx in range(args.num_users):        
+#     loss, acc = clients[idx].eval_test()
         
-    test_loss.append(loss)
-    test_acc.append(acc)
+#     test_loss.append(loss)
+#     test_acc.append(acc)
     
-    loss, acc = clients[idx].eval_train()
+#     loss, acc = clients[idx].eval_train()
     
-    train_loss.append(loss)
-    train_acc.append(acc)
+#     train_loss.append(loss)
+#     train_acc.append(acc)
 
-test_loss = sum(test_loss) / len(test_loss)
-test_acc = sum(test_acc) / len(test_acc)
+# test_loss = sum(test_loss) / len(test_loss)
+# test_acc = sum(test_acc) / len(test_acc)
 
-train_loss = sum(train_loss) / len(train_loss)
-train_acc = sum(train_acc) / len(train_acc)
+# train_loss = sum(train_loss) / len(train_loss)
+# train_acc = sum(train_acc) / len(train_acc)
 
-print(f'Train Loss: {train_loss}, Test_loss: {test_loss}')
-print(f'Train Acc: {train_acc}, Test Acc: {test_acc}')
+# print(f'Train Loss: {train_loss}, Test_loss: {test_loss}')
+# print(f'Train Acc: {train_acc}, Test Acc: {test_acc}')
 
-print(f'Best Clients AVG Acc: {np.mean(clients_best_acc)}')
+# print(f'Best Clients AVG Acc: {np.mean(clients_best_acc)}')
 
-for jj in range(len(clusters)):
-    print(f'Cluster {jj}, Best Glob Acc {best_glob_acc[jj]:.3f}')
+# for jj in range(len(clusters)):
+#     print(f'Cluster {jj}, Best Glob Acc {best_glob_acc[jj]:.3f}')
 
-print(f'Average Best Glob Acc {np.mean(best_glob_acc[0:len(clusters)]):.3f}')
+# print(f'Average Best Glob Acc {np.mean(best_glob_acc[0:len(clusters)]):.3f}')
 
-############################# Saving Print Results 
-with open(path+str(args.cluster_alpha)+'_final_results.txt', 'a') as text_file:
-    print(f'Train Loss: {train_loss}, Test_loss: {test_loss}', file=text_file)
-    print(f'Train Acc: {train_acc}, Test Acc: {test_acc}', file=text_file)
+# ############################# Saving Print Results 
+# with open(path+str(args.cluster_alpha)+'_final_results.txt', 'a') as text_file:
+#     print(f'Train Loss: {train_loss}, Test_loss: {test_loss}', file=text_file)
+#     print(f'Train Acc: {train_acc}, Test Acc: {test_acc}', file=text_file)
 
-    print(f'Best Clients AVG Acc: {np.mean(clients_best_acc)}', file=text_file)
-    print(f'Best Global Models AVG Acc: {np.mean(best_glob_acc[0:len(clusters)])}', file=text_file)
+#     print(f'Best Clients AVG Acc: {np.mean(clients_best_acc)}', file=text_file)
+#     print(f'Best Global Models AVG Acc: {np.mean(best_glob_acc[0:len(clusters)])}', file=text_file)
 
     
